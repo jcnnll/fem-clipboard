b56b669ecafad4cfde8a231432d9f3f12215c440
jcnnll <201615143+jcnnll@users.noreply.github.com>
2026-01-13 13:15:55 +0200

    chore: add astro config for GHP deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,12 @@ import { fileURLToPath } from "node:url";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://jcnnll.github.io",
+  base: "/fem-clipboard/",
+
+  // Output directory for the build
+  outDir: "docs",
+
   vite: {
     resolve: {
       alias: {
